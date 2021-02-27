174447757bff4b2c07658a1762f350eb9c4f8bca
decka <steriuandreea@yahoo.ro>
2021-02-27 16:51:53 +0100

    knex file updated

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,14 +1,14 @@
-// const { database, username, password } = require(__dirname +
-//   "server/config/dbConfig");
+const { database, username, password } = require(__dirname +
+  "/config/dbConfig");
 const { knexSnakeCaseMappers } = require("objection");
 
 module.exports = {
   development: {
     client: "pg",
     connection: {
-      database: "planday_solution",
-      username: "decka",
-      password: "",
+      user: process.env.DB_USER || username,
+      password: process.env.DB_USER_PASS || password,
+      database: process.env.DB || database,
     },
   },
 
