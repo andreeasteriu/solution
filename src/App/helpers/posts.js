c4ee331d7d6a9638da8e8d307a291a123485691d
decka <steriuandreea@yahoo.ro>
2021-02-28 13:49:58 +0100

    changed posts back

--- src/App/helpers/posts.js
+++ src/App/helpers/posts.js
@@ -1,9 +1,9 @@
 // ====================== PRODUCTION ======================
-export const endpoint =
-  "http://ec2-13-48-148-107.eu-north-1.compute.amazonaws.com/api";
+// export const endpoint =
+//   "http://ec2-13-48-148-107.eu-north-1.compute.amazonaws.com/api";
 
 // ====================== DEVELOPMENT ======================
-// export const endpoint = "http://localhost:8080/api/posts";
+export const endpoint = "http://localhost:8080/api/posts";
 
 export const getFeedPosts = async () => {
   try {
